359c13f58ffe7dde68859639e5c8faae6f47012c
Arsalan Vakili <arsenal.v.001@gmail.com>
2015-08-16 11:35:44 -0400

    swift commit

--- TipCalculatorInSwift/ViewController.swift
+++ TipCalculatorInSwift/ViewController.swift
@@ -9,17 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var billAmountTextField: UITextField!
+    
+    @IBOutlet weak var rangeOfTipPercentage: UISlider!
+    @IBOutlet weak var calculatedTip: UILabel!
+    
+    @IBOutlet weak var sliderSwitchLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func calculateTipSlider(sender: AnyObject) {
+        var formatter : NSNumberFormatter = NSNumberFormatter()
+        formatter.numberStyle = NSNumberFormatterStyle.DecimalStyle
+        
+        var billTotal = formatter.numberFromString(billAmountTextField.text)
+        
+        
+        if let unwrappedTotal = billTotal{
+            
+            let billAmountEntered = unwrappedTotal.floatValue
+            
+            let tipRange = floor(rangeOfTipPercentage.value)
+            
+            let totalTip = billAmountEntered * tipRange / 100.0
 
-
+            sliderSwitchLabel.text = "\(tipRange)"
+            calculatedTip.text = "\(totalTip)"
+            
+        } else {
+            
+            calculatedTip.text = "Please enter a valid number"
+        }
+        
+        
+    }
+    
+    
 }
+
+
+
+
+
+
 
